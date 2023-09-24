be34a62f4c2b855635e03999eed700665df43920
9Nyw1988 <cool.9nyw@gmail.com>
2023-09-24 10:19:25 +0300

    add func exel report

--- R4C/robots/views.py
+++ R4C/robots/views.py
@@ -1,7 +1,8 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
-
+from django.http import HttpResponse, HttpRequest
 from .models import Robot
+from .services import create_data_to_excel
 
 
 class RobotAddView(CreateView):
@@ -18,3 +19,14 @@ class RobotListView(ListView):
     Класс получения списка существующих роботов.
     """
     queryset = Robot.objects.all()
+
+
+def week_exel_report(request: HttpRequest) -> HttpResponse:
+    """
+    Функция получения Excel отчёта.
+    """
+    data = create_data_to_excel()
+    response = HttpResponse(data.read(), content_type="application/vnd.openxmlformats-officedocument.spreadsheetml"
+                                                      ".sheet")
+    response['Content-Disposition'] = 'attachment; filename=week_robots_report.xlsx'
+    return response
