c0ded94a4ddb93455f2fe14656dbc26461a5f94a
9Nyw1988 <cool.9nyw@gmail.com>
2023-09-24 14:47:48 +0300

    fix time week ago

--- R4C/robots/services.py
+++ R4C/robots/services.py
@@ -1,19 +1,20 @@
+import datetime
 import io
 from django.db.models import Count
-from django.utils import timezone
-from datetime import timedelta
 from .models import Robot
 from openpyxl import Workbook
+from datetime import timedelta
+from django.utils import timezone
 
 
 def create_data_to_excel():
     """
     Функция создает отчет в формате Excel по моделям роботов.
     """
-    # Получаем текущее время
-    time_now = timezone.now()
+
     # Получаем время неделю назад
-    time_one_week_ago = timezone.now() - timedelta(days=7)
+    time_one_week_ago = timezone.now().date() - timedelta(days=7)
+    time_one_week_ago = time_one_week_ago.strftime('%Y-%m-%d %H:%M:%S')
 
     # Инициализируем объекты Workbook и BytesIO
     output = io.BytesIO()
@@ -21,13 +22,12 @@ def create_data_to_excel():
 
     # Получаем список моделей роботов
     models_list = Robot.objects.values_list('model', flat=True).distinct()
-    # Генермруем страницу с зоголовками м записями
+    # Генерируем страницу с зоголовками и записями
     for model in models_list:
         ws = wb.create_sheet(model)
         ws.append(['Модель', 'Версия', 'Количество за неделю'])
-        queryset = Robot.objects.filter(model=model, created__gte=time_one_week_ago, created__lte=time_now).values(
-            'version').annotate(
-            total=Count('version'))
+        queryset = Robot.objects.filter(model=model).exclude(created__gt=time_one_week_ago).values(
+            'version').annotate(total=Count('version'))
         # Записываем полученную информацию в файл Excel
         for item in queryset:
             ws.append([model, item['version'], item['total']])
--- R4C/robots/urls.py
+++ R4C/robots/urls.py
@@ -7,5 +7,5 @@ app_name = 'robots'
 urlpatterns = [
     path("add-robot/", RobotAddView.as_view(), name="robot_add"),
     path("robots-list/", RobotListView.as_view(), name="robots_list"),
-    path("week-report", week_exel_report),
+    path("week-report/", week_exel_report),
 ]
